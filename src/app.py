7ef76b1b0cb0b035964b2f0753e0a0ac5c99958c
minho <minhob38@gmail.com>
2023-06-18 17:01:06 +0900

    yfinance install

--- src/app.py
+++ src/app.py
@@ -5,34 +5,34 @@ from package.rsi import add_rsi, add_rsi_trade_point, get_backtest_rsi
 from package.scraping import get_daily_price
 from package.inf_trade import get_backtest
 
-## table로 변수 하나 만들자
-## naver에서 주가 scraping
-df_technical_analyis = get_daily_price_from_naver()
+# ## table로 변수 하나 만들자
+# ## naver에서 주가 scraping
+# df_technical_analyis = get_daily_price_from_naver()
 
-## macd 계산
-add_macd(df_technical_analyis, short=12, long=26)
-add_macd_signal(df_technical_analyis, span=9)
-add_macd_oscillator(df_technical_analyis)
-add_macd_slope(df_technical_analyis)
-add_macd_trade_point(df_technical_analyis)
+# ## macd 계산
+# add_macd(df_technical_analyis, short=12, long=26)
+# add_macd_signal(df_technical_analyis, span=9)
+# add_macd_oscillator(df_technical_analyis)
+# add_macd_slope(df_technical_analyis)
+# add_macd_trade_point(df_technical_analyis)
 
-## rsi 계산
-add_rsi(df_technical_analyis)
-add_rsi_trade_point(df_technical_analyis)
+# ## rsi 계산
+# add_rsi(df_technical_analyis)
+# add_rsi_trade_point(df_technical_analyis)
 
-# backtest 로직은 더 만들어야 함...
-df_rsi_backtest = get_backtest_rsi(df_technical_analyis)
-# print(df_rsi_backtest)
+# # backtest 로직은 더 만들어야 함...
+# df_rsi_backtest = get_backtest_rsi(df_technical_analyis)
+# # print(df_rsi_backtest)
 
-# 최종결과물 매수/매도 포인트 정보가 있는 엑셀, 매일 매일
-# 우선 개별종목 분석 부터...
-# print(df_technical_analyis.columns)
+# # 최종결과물 매수/매도 포인트 정보가 있는 엑셀, 매일 매일
+# # 우선 개별종목 분석 부터...
+# # print(df_technical_analyis.columns)
 
-# ETF x3 Leverage Ticker
-# FNGU
-# TQQQ
+# # ETF x3 Leverage Ticker
+# # FNGU
+# # TQQQ
 
 
-df_etf = get_daily_price("SOXL")
-get_backtest(df_etf)
-print(df_etf)
+# df_etf = get_daily_price("SOXL")
+# get_backtest(df_etf)
+# print(df_etf)
